a19362a9520cd37c9a688ceff4db9a0181f6e6e7
DevangPhadnis <devangphadnis3@gmail.com>
2025-05-23 00:35:29 +0530

    Time: 143 ms (13.41%), Space: 80.5 MB (14.41%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int dp[][][] = new int[prices.length][2][3];
+        for(int[][] dpArr: dp) {
+            for(int[] dparr: dpArr)
+                Arrays.fill(dparr, -1);
+        }
+        return helper(prices, dp, 0, 1, 2);
+    }
+
+    public int helper(int[] prices, int dp[][][], int index, int buy, int capacity) {
+        if(index >= prices.length || capacity == 0) return 0;
+
+        if(dp[index][buy][capacity] != -1) return dp[index][buy][capacity];
+        int profit = 0;
+        if(buy == 1) {
+            int pick = helper(prices, dp, index+1, 0, capacity) - prices[index];
+            int notpick = helper(prices, dp, index+1, 1, capacity);
+
+            profit = Math.max(pick, notpick);
+        }
+        else {
+            int pick = helper(prices, dp, index+1, 1, capacity - 1) + prices[index];
+            int notpick = helper(prices, dp, index+1, 0, capacity);
+
+            profit = Math.max(pick, notpick);
+        }
+
+        dp[index][buy][capacity] = profit;
+        return dp[index][buy][capacity];
+    }
+}
