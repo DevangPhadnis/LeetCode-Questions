7737c4a2b7037ea584a7d3c72e20c61e9414a0a8
DevangPhadnis <devangphadnis3@gmail.com>
2025-05-23 00:35:06 +0530

    Time: 41 ms (6.66%), Space: 45.7 MB (26.06%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minInsertions(String s) {
+        String s1 = "";
+        for(int i=s.length() - 1;i>=0;i--) {
+            s1 = s1 + s.charAt(i);
+        }
+        int dp[][] = new int[s.length()][s1.length()];
+        for(int[] dpArr: dp)
+            Arrays.fill(dpArr, -1);
+        return s.length() - helper(s, s1, s.length() - 1, s1.length() - 1, dp);
+    }
+
+    public int helper(String text1, String text2, int len1, int len2, int dp[][]) {
+        if(len1 == -1 || len2 == -1) return 0;
+
+        if(dp[len1][len2] != -1) return dp[len1][len2];
+
+        if(text1.charAt(len1) == text2.charAt(len2)) {
+            dp[len1][len2] = 1 + helper(text1, text2, len1-1, len2-1, dp);
+            return dp[len1][len2];
+        }
+        else {
+            int operation1 = helper(text1, text2, len1-1, len2, dp);
+            int operation2 = helper(text1, text2, len1, len2-1, dp);
+
+            dp[len1][len2] = Math.max(operation1, operation2);
+            return dp[len1][len2];
+        }
+    }
+}
