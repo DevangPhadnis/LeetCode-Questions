edd1720cad68654b9d051fc6a9610807e12fe6ca
DevangPhadnis <devangphadnis3@gmail.com>
2025-06-02 22:20:17 +0530

    Time: 0 ms (100%), Space: 41.8 MB (69.11%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -17,16 +17,14 @@ class Solution {
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
 
-        return solve(root.left, root.right);
+        return helper(root.left, root.right);
     }
 
-    public boolean solve(TreeNode leftNode, TreeNode rightNode) {
-        if(leftNode == null && rightNode == null) {
-            return true;
-        }
+    public boolean helper(TreeNode left, TreeNode right) {
+        if(left == null && right == null) return true;
 
-        if(leftNode == null || rightNode == null) return false;
+        if(left == null || right == null) return false;
 
-        return leftNode.val == rightNode.val && solve(leftNode.left, rightNode.right) && solve(leftNode.right, rightNode.left);
+        return left.val == right.val && helper(left.left, right.right) && helper(left.right, right.left);
     }
 }
