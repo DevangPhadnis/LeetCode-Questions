b6e770eec0d2fdca68344dda7e7f968c2251ea66
DevangPhadnis <devangphadnis3@gmail.com>
2025-06-02 22:56:54 +0530

    Time: 0 ms (100%), Space: 45.5 MB (9.29%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -18,24 +18,21 @@ class Solution {
         int ans[] = new int[1];
         ans[0] = Integer.MIN_VALUE;
         helper(root, ans);
-        return ans[0] - 1;
+        return ans[0]-1;
     }
 
     public int helper(TreeNode root, int ans[]) {
         if(root == null) return 0;
 
-        int lh = helper(root.left, ans);
-        int rh = helper(root.right, ans);
+        int ls = helper(root.left, ans);
+        int rs = helper(root.right, ans);
 
+        int excluded = ls + rs + 1;
 
-        // Include -- Root is excluded so we can take both lh, rh
-        int rootIsExcluded = lh + rh + 1;
+        int included = Math.max(ls, rs) + 1;
 
-        // Exlude -- Root is included, we will take only the max of lh or rh
-        int rootIsIncluded = Math.max(lh, rh) +1;
+        ans[0] = Math.max(ans[0], Math.max(excluded, included));
 
-        ans[0] = Math.max(ans[0], Math.max(rootIsExcluded, rootIsIncluded));
-
-        return rootIsIncluded;
+        return included;
     }
 }
