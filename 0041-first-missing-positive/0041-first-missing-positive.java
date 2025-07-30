c4414ced63345c6e173a2b738e7d277840e2baa7
DevangPhadnis <devangphadnis3@gmail.com>
2025-07-30 22:26:27 +0530

    Time: 1 ms (100%), Space: 57 MB (73.67%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,6 +1,6 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashMap<Integer, Boolean> map = new HashMap<>();
+        /* HashMap<Integer, Boolean> map = new HashMap<>();
         int max = Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++) {
             map.put(nums[i], true);
@@ -13,6 +13,27 @@ class Solution {
             }
         }
 
-        return max+1;
+        return max+1; */
+
+        int n = nums.length;
+
+        for(int i=0;i<n;i++) {
+            if(nums[i] <= 0 || nums[i] > n) {
+                nums[i] = n+1;
+            }
+        }
+
+        for(int i=0;i<n;i++) {
+            int value = Math.abs(nums[i]);
+            if(value <= n && nums[value-1] > 0) {
+                nums[value-1] = -nums[value-1];
+            }
+        }
+
+        for(int i=0;i<n;i++) {
+            if(nums[i]>0) return i+1;
+        }
+
+        return n+1;
     }
 }
