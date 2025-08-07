a9331b212c6e5ce1d5c7a762b168d3ab0457928f
DevangPhadnis <devangphadnis3@gmail.com>
2025-08-07 23:57:30 +0530

    Time: 346 ms (50.14%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -2,19 +2,19 @@
 with rowCount as (select count(*) as totalRows from Seat),
 updatedSeat as (select id, student, row_number() over (order by id) as rowNumber from Seat)
 select 
-    CASE
-        WHEN (SELECT totalRows FROM rowCount) % 2 = 0 then
-            CASE
-                WHEN id % 2= 1 THEN id+1
-                WHEN id % 2 = 0 THEN id-1
-                ELSE id
+    case
+        when (select totalRows from rowCount) % 2 = 0 then
+            case
+                when id % 2= 1 then id+1
+                when id % 2 = 0 then id-1
+                else id
             END
-        WHEN (SELECT totalRows FROM rowCount) % 2 = 1 and rowNumber < (select totalRows from rowCount) then
-            CASE
-                WHEN id % 2= 1 THEN id+1
-                WHEN id % 2 = 0 THEN id-1
-                ELSE id
+        when (select totalRows from rowCount) % 2 = 1 and rowNumber < (select totalRows from rowCount) then
+            case
+                when id % 2= 1 then id+1
+                when id % 2 = 0 then id-1
+                else id
             END
-        ELSE id
-    END AS id, student
-FROM updatedSeat order by id;
+        else id
+    END as id, student
+from updatedSeat order by id;
